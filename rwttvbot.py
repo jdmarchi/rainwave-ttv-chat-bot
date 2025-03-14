f0195b626e663cadb6bb3e956d9ae6b2a13d8583
jdmarchi <jdylanmarchi@gmail.com>
2025-03-14 16:22:09 -0400

    First working version!

--- rwttvbot.py
+++ rwttvbot.py
@@ -4,11 +4,11 @@ import websocket
 import json
 
 
-TTV_BOT_USER_ID = "YOUR_BOT_USER_ID"
-TTV_OAUTH_TOKEN = "YOUR_OAUTH_TOKEN"
-TTV_CLIENT_ID = "YOUR_CLIENT_ID"
+TTV_BOT_USER_ID = "BOT_USER_ID_HERE"
+TTV_OAUTH_TOKEN = "OAUTH_TOKEN_HERE"
+TTV_CLIENT_ID = "CLIENT_ID_HERE"
 
-CHAT_CHANNEL_USER_ID = "BROADCASTER_USER_ID"
+CHAT_CHANNEL_USER_ID = "BROADCASTER_ID_HERE"
 
 EVENTSUB_WEBSOCKET_URL = "wss://eventsub.wss.twitch.tv/ws"
 
@@ -26,7 +26,7 @@ else:
     print("User Access Token validated.")
 print(response.content.decode('utf-8'))
 
-response = requests.post('https://id.twitch.tv/oauth2/token', data={"client_id":TTV_CLIENT_ID,"client_secret":"YOUR_CLIENT_SECRET","grant_type":"client_credentials"})
+response = requests.post('https://id.twitch.tv/oauth2/token', data={"client_id":TTV_CLIENT_ID,"client_secret":"CLIENT_SECRET_HERE","grant_type":"client_credentials"})
 response_json = response.json()
 print(response_json)
 if "access_token" in response_json:
@@ -46,13 +46,40 @@ def on_open(ws):
     print(f"Websocket connection opened to {EVENTSUB_WEBSOCKET_URL}")
 
 def on_message(ws, message):
-    print("Message received")
     msg_json = json.loads(message)
     print(msg_json["metadata"]["message_type"])
     if msg_json["metadata"]["message_type"] == "session_welcome":
         print("session_welcome received. Calling registerEventSubListener.")
         registerEventSubListener(msg_json["payload"]["session"]["id"])
-    
+    elif msg_json["metadata"]["message_type"] == "notification":
+        print("Possible chat received.")
+        print(msg_json["payload"]["event"]["message"]["text"])
+        if msg_json["payload"]["event"]["message"]["text"] == "!song":
+            print("Song request received.")
+            print("Fetching song info from Rainwave")
+            song_request = requests.get('https://rainwave.cc/api4/info_all?sid=2')
+            print("Parsing to JSON")
+            song_json = song_request.json()
+            song_name = song_json["all_stations_info"]["2"]["title"]
+            print(song_name)
+            print("Getting artists...")
+            artist_name = song_json["all_stations_info"]["2"]["artists"]
+            print(artist_name)
+            link_request = requests.get('https://rainwave.cc/api4/info?sid=2')
+            link_json = link_request.json()
+            song_link = link_json["sched_current"]["songs"][0]["url"]
+            print(song_link)                                        
+            sendChatMessage(f"Now playing: {song_name} - {artist_name}. Get @ OCR: {song_link}")
+
+def sendChatMessage(message):
+    print("sendChatMessage called")
+    print("Constructing data for POST request")
+    chat_data = {
+        "broadcaster_id": CHAT_CHANNEL_USER_ID,
+        "sender_id": CHAT_CHANNEL_USER_ID,
+        "message": message
+    }
+    chat_request = requests.post('https://api.twitch.tv/helix/chat/messages', headers=ttv_headers, json=chat_data)
 
 def registerEventSubListener(session_ID):
     print("registerEventSubListener called")
@@ -62,7 +89,7 @@ def registerEventSubListener(session_ID):
         "version": "1",
         "condition": {
             "broadcaster_user_id": CHAT_CHANNEL_USER_ID,
-            "user_id": TTV_BOT_USER_ID
+            "user_id": CHAT_CHANNEL_USER_ID
         },
         "transport": {
             "method": "websocket",
@@ -70,7 +97,7 @@ def registerEventSubListener(session_ID):
         }
     }
     print("Sending post request...")
-    es_response = requests.post('https://api.twitch.tv/helix/eventsub/subscriptions', headers=ttv_app_headers, json=es_data)
+    es_response = requests.post('https://api.twitch.tv/helix/eventsub/subscriptions', headers=ttv_headers, json=es_data)
     print(es_response)
     if es_response.status_code != 202:
         print(f"Failed to subscribe. Status code {es_response.status_code}")
@@ -85,5 +112,3 @@ print("Websocket App Created")
 print("Running websocket client...")
 ttv_websocketClient.run_forever()
 print("Websocket client closed.")
-
-
